d88ca5808b48504f452ee3c1cf791cbb5bd4459e
Marianna Costanza <mcostanza98@gmail.com>
2023-02-13 18:42:17 +0100

    Correcciones backend

--- db/initDB.js
+++ db/initDB.js
@@ -1,4 +1,4 @@
-const { getConnection } = require('./db');
+const db = require('./db');
 
 require('dotenv').config();
 
@@ -6,47 +6,48 @@ async function main(){
     let connection;
 
     try{
-        connection = await getConnection();
+        connection = await db.getConnection();
 
-        console.log('Borrando tablas existentes')
-        await connection.query('DROP TABLE IF EXIST users')
-        await connection.query('DROP TABLE IF EXIST photo')
-        await connection.query('DROP TABLE IF EXIST like')
-        await connection.query('DROP TABLE IF EXIST comment')
-        //console.log('Creating tables');
+        //console.log('Borrando tablas existentes')
+      //  await connection.query('DROP TABLE IF EXIST users')
+      //  await connection.query('DROP TABLE IF EXIST photo')
+      //  await connection.query('DROP TABLE IF EXIST like')
+       // await connection.query('DROP TABLE IF EXIST comment')
+       // console.log('Creating tables');
 
-       // await connection.query(
-        //    CREATE TABLE users (
-        //        id INTEGRED PRIMARY KEY AUTO_INCREMEN,
-        //        email VARCHAR(100) UNIQUE NOT NULL,
-        //        name VARCHAR(50) NOT NULL,
-        //        surname VARCHAR(50) NOT NULL,
-        //        password VARCHAR(100) NOT NULL,
-        //        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        //    );
-        //   );
-        // CREATE TABLE Photo (
-        //        id INTEGRED PRIMARY KEY AUTO_INCREMEN,
-        //        user_id INTEGER NOT NULL,
-        //        image (?),
-        //        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        //        FOREIGN KEY (user_id) REFERENCES users(id)
-        //    );
-        // CREATE TABLE Like (
-        //        id INTEGRED PRIMARY KEY AUTO_INCREMEN,
-        //        user_id VARCHAR(100) UNIQUE NOT NULL,
-        //        photo_id VARCHAR(100) UNIQUE NOT NULL,
-        //        FOREIGN KEY (user_id) REFERENCES users(id),
-        //       
-        //    );
-        // CREATE TABLE Comment (
-        //        id INTEGRED PRIMARY KEY AUTO_INCREMEN,
-        //        user_id VARCHAR(100) UNIQUE NOT NULL,
-        //        text VARCHAR(200) NOT NULL,
-        //        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        //        FOREIGN KEY (user_id) REFERENCES users(id)
-        //    );
-        //   );
+        await connection.query(`
+            CREATE TABLE users (
+                id INTEGRED PRIMARY KEY AUTO_INCREMEN,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                name VARCHAR(50) NOT NULL,
+                surname VARCHAR(50) NOT NULL,
+               password VARCHAR(100) NOT NULL,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            );
+           
+         CREATE TABLE Photo (
+                id INTEGRED PRIMARY KEY AUTO_INCREMEN,
+                user_id INTEGER NOT NULL,
+                image VARCHAR(100),
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            );
+         CREATE TABLE Like (
+                id INTEGRED PRIMARY KEY AUTO_INCREMEN,
+                user_id VARCHAR(100) UNIQUE NOT NULL,
+                photo_id VARCHAR(100) UNIQUE NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users(id),
+               
+            );
+         CREATE TABLE Comment (
+                id INTEGRED PRIMARY KEY AUTO_INCREMEN,
+                user_id VARCHAR(100) UNIQUE NOT NULL,
+                text VARCHAR(200) NOT NULL,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            );
+           );
+        `);
 
     }catch(error){
     console.error(error);
