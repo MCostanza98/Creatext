f9a2ff49ca410524fc8300eac7b1a7cf0e650793
Marianna Costanza <mcostanza98@gmail.com>
2023-02-18 18:34:11 +0100

    Rutas

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ const {
     loginController,
 } = require('./controllers/users');
 
+const {
+    getPhotoController,
+    newPhotoController,
+    getSinglePhotoController,
+    deletePhotoController,
+} = require('./controllers/photo');
+
+const {
+    newLikeController,
+    getLikeController,
+    likeController,
+    deleteLikeController,
+} = require('./controllers/like');
+
+const {
+    getCommentController,
+    newCommentController,
+    getSingleCommentController,
+    deleteCommentController,
+} = require('./controllers/comment');
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -19,12 +40,24 @@ app.post('/users', newUserController);
 app.get('user/:id', getUserController);
 app.post('/login', loginController);
 
-//Rutas users
+//Rutas photos
 
 app.get('/', getPhotoController);
 app.post('/', newPhotoController);
-app.get('/photo/:id', getPhotoController);
+app.get('/photo/:id', getSinglePhotoController);
 app.delete('/photo/:id', deletePhotoController);
+
+//Rutas likes
+app.post('/like', newLikeController);
+app.get('like/:id', getLikeController);
+app.post('/like', likeController);
+app.delete('/like/:id', deleteLikeController);
+
+//Rutas Comment
+app.get('/', getCommentController);
+app.post('/', newCommentController);
+app.get('/comment/:id', getSingleCommentController);
+app.delete('/comment/:id', deleteCommentController);
 
 
 //middleware de 404
