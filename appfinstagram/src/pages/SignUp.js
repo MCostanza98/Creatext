6c860913255b2b1739f595bd334feda1bf5d177f
Raquel laPaz <lunablankaa25@hotmail.es>
2023-03-01 00:43:24 +0100

    rutas

--- appfinstagram/src/pages/SignUp.js
+++ appfinstagram/src/pages/SignUp.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState}  from "react";
+import{ Link } from 'react-router-dom';
+
+import gif from '../assets/mobilegif.gif';
+
+
+import{  Container, Gif, FromContainer, Form, Footer } from './stylesSingUp';
 
 
 
 const SignUp = () => {
-   return <h1>SignUp</h1>
+    const [input, setInput] = useState('');
+   return(
+    <Container>
+        <Gif src={gif} alt="gif" />
+
+        <FromContainer>
+         <Form>
+
+            
+            <span>Registrate para ver fotos</span>
+
+            <hr />
+            <input value={input} name="name" onChange={(e)=> setInput(e.target.value)} />
+
+            <button type="submit">Registrate</button>
+
+            <hr />
+
+            <span className="footer">Aceptar condiciones de política de datos.</span>
+         </Form>
+
+         <Footer>
+            <p>
+                ¿Tienes una cuenta?<Link to = "signin">Entrar</Link>
+            </p>
+         </Footer>
+       </FromContainer>
+    </Container>
+   )
 }
 
 export default SignUp;
