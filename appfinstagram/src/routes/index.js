a400f6fde80f8a6b96b4ac96fa2cc25dabcccee0
Raquel laPaz <lunablankaa25@hotmail.es>
2023-03-05 05:10:57 +0100

    ruta modificada

--- appfinstagram/src/routes/index.js
+++ appfinstagram/src/routes/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import{ ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReacToastify.css';
+import { ToastContainer } from 'react-toastify';
 
-import 'react-toastify/dist/ReactToastify.css';
+
 
 import Main from "../pages/Main";
 import SignIn from "../pages/SignIn";
